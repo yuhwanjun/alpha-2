dffcdecc234c590dde7134805ccdb98d6b3aa963
yuhwanjun <yuhwanjun0311@gmail.com>
2023-10-10 13:55:00 +0900

    update

--- my-vue-project/server/server.js
+++ my-vue-project/server/server.js
@@ -13,10 +13,42 @@ app.use((req, res, next) => {
   next();
 });
 
-// 클라이언트로부터의 요청을 처리하는 엔드포인트
+//! ISBN 검색 엔드포인트
+app.get('/api/isbn', async (req, res) => {
+  try {
+    const { ttbkey, itemId } = req.query;
+    const response = await axios.get(`http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx`, {
+      params: {
+        ttbkey,
+        itemIdType: 'ISBN',
+        ItemId: itemId,
+        output: 'js',
+        Version: '20131101',
+        Cover: "Big",
+        OptResult: 'previewImgList,packing,ratingInfo,previewImgList,reviewList,fulldescription,Toc,Story',
+        // https://docs.google.com/forms/d/e/1FAIpQLSfjynYb6jp0q8IMGJ9jlVUmnnGMfq8HLDUp54lJM6naXq42tg/viewform?vc=0&c=0&w=1&flr=0
+        // 목차나 줄거리는 프리미엄 api 심사를 통해야 받을 수 있을 듯
+      },
+    });
+
+    // 서버에서 받아온 도서 정보를 바로 클라이언트에 전달
+    res.send(response.data);
+  } catch (error) {
+    console.error('ISBN 검색 중 오류 발생:', error);
+    res.status(500).json({ error: '서버 오류' });
+  }
+});
+
+// 리스트 검색 엔드포인트
 app.get('/api/data', async (req, res) => {
   try {
-    const response = await axios.get('http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbyuhwanjun1232001&QueryType=ItemNewAll&MaxResults=100&start=1&SearchTarget=Book&output=js&Version=20131101&CategoryId=50993');
+    // 클라이언트에서 설정한 쿼리 매개변수를 서버에서 받아옴
+    const { ttbkey, QueryType, MaxResults, start, SearchTarget, output, Version, CategoryId, Cover } = req.query;
+
+    // 서버에서 받아온 쿼리 매개변수를 외부 API 요청에 전달
+    const response = await axios.get(
+      `http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${ttbkey}&QueryType=${QueryType}&MaxResults=${MaxResults}&start=${start}&SearchTarget=${SearchTarget}&output=${output}&Version=${Version}&CategoryId=${CategoryId}&Cover=${Cover}`);
+    
     const data = response.data;
     res.json(data);
   } catch (error) {
--- my-vue-project/src/App.vue
+++ my-vue-project/src/App.vue
@@ -1,9 +1,29 @@
 <template>
   <div>
+    <div class="bookInfo" v-if="bookInfo">
+      <h2>{{ bookInfo.title }}</h2>
+      <h3>지은이: {{ bookInfo.author }}</h3>
+      <img v-if="bookInfo.cover" :src="bookInfo.cover" alt="책 표지 이미지" />
+      <a :href="bookInfo.link">알라딘 이동</a>
+      <p>설명: {{ bookInfo.description }}</p>
+      <p>카테고리: {{ bookInfo.categoryName }}</p>
+      <p>페이지 수: {{ bookInfo.subInfo.itemPage }}</p>
+      <p>무게: {{ bookInfo.subInfo.packing.weight }}</p>
+      <p>별점: {{ bookInfo.subInfo.ratingInfo.ratingScore }}</p>
+      <!-- <pre>{{ bookInfo }}</pre> -->
+    </div>
+    <h2>도서 정보:</h2>
     <button @click="fetchData">데이터 가져오기</button>
-    <div v-if="responseData">
-      <h2>서버로부터 받은 데이터:</h2>
-      <pre>{{ responseData }}</pre>
+    <div class="book-wrap" v-if="bookData">
+      <div class="book-item" v-for="(book, index) in bookData.item" :key="index">
+        <div @click="testData(book.isbn)">
+          <p><strong>제목:</strong> {{ book.title }}</p>
+          <p><strong>저자:</strong> {{ book.author }}</p>
+          <p><strong>출판일:</strong> {{ book.pubDate }}</p>
+          <p><strong>가격:</strong> {{ book.priceSales }}원</p>
+          <p><strong>줄거리:</strong> {{ book.description }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,13 +35,43 @@ export default {
   data() {
     return {
       responseData: null,
+      bookData: null,
+      bookInfo: null,
     };
   },
   methods: {
     async fetchData() {
       try {
-        const response = await axios.get('http://localhost:3000/api/data'); // 서버의 경로
-        this.responseData = response.data;
+        const params = {
+          ttbkey: 'ttbyuhwanjun1232001',
+          QueryType: 'Bestseller',
+          MaxResults: 10,
+          start: 1,
+          SearchTarget: 'Book',
+          output: 'js',
+          Version: '20131101',
+          CategoryId: 0
+          //? https://docs.google.com/document/d/1mX-WxuoGs8Hy-QalhHcvuV17n50uGI2Sg_GHofgiePE/edit
+        };
+
+        const response = await axios.get('http://localhost:3000/api/data', { params }); // 서버의 경로와 params 설정
+        this.bookData = response.data; // 받아온 데이터를 responseData에 저장
+        console.log(response.data)
+      } catch (error) {
+        console.error('데이터를 가져오는 중 오류 발생:', error);
+      }
+    },
+    async testData(itemId) {
+      try {
+        const params = {
+          ttbkey: 'ttbyuhwanjun1232001',
+          itemId: itemId,
+        };
+
+        const response = await axios.get('http://localhost:3000/api/isbn', { params }); // 서버의 경로와 params 설정
+        this.responseData = response.data; // 받아온 데이터를 responseData에 저장
+        this.bookInfo = this.responseData.item[0];
+        console.log(this.bookInfo)
       } catch (error) {
         console.error('데이터를 가져오는 중 오류 발생:', error);
       }
@@ -31,5 +81,31 @@ export default {
 </script>
 
 <style>
-/* 필요한 스타일을 추가할 수 있습니다. */
+*{
+  margin: 0;
+  padding: 0;
+}
+  .book-wrap {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    align-content: flex-start;
+  }
+  .book-item {
+    flex: 1 1 30%;
+    padding: 20px;
+  }
+  .book-item div{
+    cursor: pointer;
+    border: 1px solid pink;
+    padding: 10px;
+    border-radius: 4px;
+  }
+  .bookInfo {
+    width: 65%;
+  }
+  .bookInfo p{
+    padding-bottom: 1em;
+  }
+  /* 필요한 스타일을 추가할 수 있습니다. */
 </style>
